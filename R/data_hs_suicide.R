abe0c84e8c88b167df2ed48c0a037a998b73f730
Estephania Plascencia <evill092@fiu.edu>
2022-07-31 18:51:29 -0400

    Correction

--- R/data_hs_suicide.R
+++ R/data_hs_suicide.R
@@ -15,7 +15,7 @@ NULL
 #' This is a subset of the Combined YRBS High School Data Set
 #' @format A tibble with 504,249 rows and 6 variables
 #' \describe{
-#' 
+#' \item {record} {a character variable for the ID number of the subject}
 #' \item {is_hopeless} {a Boolean where TRUE is when a student answers yes to 
 #' feeling sad or hopeless (almost every day for 2 or more weeks in a row so 
 #' that they stopped doing some usual activities, ever during the 12 months 
@@ -26,9 +26,9 @@ NULL
 #' \item{suicide_planned} {a Boolean where TRUE is when a student answers 
 #' yes to having made a plan about how they would attempt suicide (during the 
 #' 12 months before the survey)}
-#' \item {suicide_attempts} {a Boolean where TRUE is when a student answers yes
-#' to having attempted suicide (one or more times during the 12 months before 
-#' the survey)
+#' \item {suicide_attempts} an integer that indicates the times someone
+#' attempted suicide during the last 12 months. 1 = 0 times, 2 = 1 time, 3 = 2 
+#' or 3 times, 4 = 4 or 5 times, 5 = 6 or more times}
 #' \item{suicide_injury} {a Boolean where TRUE is when a student answers yes to
 #' having a suicide attempt that resulted in an injury, poisoning, or overdose
 #' that had to be treated by a doctor or nurse (during the 12 months before the
