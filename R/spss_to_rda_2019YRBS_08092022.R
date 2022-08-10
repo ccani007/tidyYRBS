835253fce5f6d178e031c454893e857928ec6698
Catalina Cañizares <catalinacanizares@Catalinas-iMac.local>
2022-08-10 19:53:42 -0400

    Cleaning 2019 data

--- R/spss_to_rda_2019YRBS_08092022.R
+++ R/spss_to_rda_2019YRBS_08092022.R
@@ -21,3 +21,49 @@ library(tidyverse)
 yrbs_2019_raw <- read_sav("inst/extData/2019_YRBS_data.dat.sav")
 saveRDS(yrbs_2019_raw , "data/yrbs_2019_raw.rda")
 
+### Cleaning variables ###
+
+yrbs_2019 <- 
+  yrbs_2019_raw %>% 
+  mutate(ID = row_number()) %>% 
+  separate(Q5, into = c("American Indian", "Asian", "Black",
+                        "Native Hawaiian or Other Pacific Islander", 
+                        "White", "Hispanic/Latino", "Multiple-Hispanic/Latino",
+                        "“Multiple – NonHispanic/Latino"), 
+           sep = "\\s"
+  ) %>% 
+  mutate(
+    Sex = case_when(
+      Q2 == 2 ~ "Male",
+      Q2 == 1 ~ "Female",
+      TRUE ~ NA_character_
+    )
+  ) %>% 
+  mutate(
+    Age = case_when(
+      Q1 == 1 ~ 12L, 
+      Q1 == 2 ~ 13L,
+      Q1 == 3 ~ 14L,
+      Q1 == 4 ~ 15L, 
+      Q1 == 5 ~ 16L, 
+      Q1 == 6 ~ 17L, 
+      Q1 == 7 ~ 18L, 
+      TRUE ~ NA_integer_
+    )
+  ) %>% 
+  mutate(
+    Grade = case_when(
+      Q3 == 1 ~ 9L,
+      Q3 == 2 ~ 10L,
+      Q3 == 3 ~ 11L,
+      Q3 == 4 ~ 12L
+      TRUE ~ NA_integer_
+    )
+  ) %>% 
+  mutate(across(c(Q23:Q27), RecodeTF))
+
+
+  
+summary(yrbs_2019)
+  
+
