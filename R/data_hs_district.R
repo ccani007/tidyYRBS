686415c2b6d178717f2495b46d66a467cdd32f5e
Estephania Plascencia <evill092@fiu.edu>
2022-08-20 20:51:00 -0400

    description of hs_district dataset

--- R/data_hs_district.R
+++ R/data_hs_district.R
@@ -1,8 +1,20 @@
-#' This is data to be included in my package
+#' This is dataset contains variables related to high school students'
+#' demographics
 #'
-#' @name hs_district
+#' @title hs_district
+#' 
+#' @descrption This dataset contains variables related to high school students’ 
+#'    demographics and risky behaviors organized by district
+
 #' @docType data
-#' @author CDC
-#' @references \url{data_blah.com}
-#' @keywords data
-NULL
+#' 
+#' @usage data(hs_district)
+#' 
+#' @author Centers for Disease Control and Prevention, Youth Risk Behavior 
+#'    Survey Data
+#'    
+#' @format A tibble with 504, 249 rows and 314 variables
+#'<https://www.cdc.gov/healthyyouth/data/yrbs/pdf/2019/2019_YRBS_SADC_Documentation.pdf>
+#'  
+#' @source <cdc.gov/healthyyouth/data/yrbs/pdf/2019/2019_YRBS_SADC_Dcumentation.pdf>
+"hs_district"
