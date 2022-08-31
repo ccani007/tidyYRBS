31aaff59a9f6fc3e8386a736f0063e7ca1594070
Catalina Cañizares <catalinacanizares@Catalinas-MacBook-Air.local>
2022-08-31 15:49:25 -0400

    Fixed indentation in description

--- R/data_yrbs_2019.R
+++ R/data_yrbs_2019.R
@@ -7,9 +7,10 @@
 #' @usage data(yrbs_2019)
 #' 
 #' @description This is the imported 2019 YRBS Data. This data set monitors
-#'  priority health risk behaviors that contribute markedly to the leading 
-#' causes of death, disability, and social problems among youth and adults in
-#' the United States. These behaviors, often include:
+#'     priority health risk behaviors that contribute markedly to the leading 
+#'     causes of death, disability, and social problems among youth and adults 
+#'     inthe United States. These behaviors, often include:
+#'     
 #' \itemize{
 #' \item{Behaviors that contribute to unintentional injuries and violence}
 #' \item{Sexual behaviors that contribute to unintended pregnancy and
